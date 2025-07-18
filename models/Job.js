3abdae16d4965e58a3669f616bb0181f5b867f7f
Kishore Kumar M <kishore7302520@gmail.com>
2025-07-18 20:11:05 +0530

    Initial commit

---
+++ models/Job.js
@@ -0,0 +1,28 @@
+// models/Job.js
+
+const mongoose = require('mongoose')
+
+const JobSchema = new mongoose.Schema({
+    company: {
+        type: String,
+        required: [true, 'Please provide a company name'],
+        maxlength: 50,
+    },
+    position: {
+        type: String,
+        required: [true, 'Please provide a position'],
+        maxlength: 100,
+    },
+    status: {
+        type: String,
+        enum: ['interview', 'declined', 'pending'],
+        default: 'pending',
+    },
+    createdBy: {
+        type: mongoose.Types.ObjectId,
+        ref: 'User',
+        required: [true, 'Please provide a user']
+    }
+}, { timestamps: true }) // timestamps gives "createdAt" and "updatedAt"
+
+module.exports = mongoose.model('Job', JobSchema)
